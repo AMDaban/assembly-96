2241713e9e4f29111859c48e4a32bc186b60e680
Alireza <daban.am56@gmail.com>
2018-03-29 20:59:30 +0430

    _get_user_input and _clear_array added

--- Projects/Calculator/calculator.asm
+++ Projects/Calculator/calculator.asm
@@ -5,6 +5,9 @@ section .data
     first_message db 'Simple Calculator: (write a simple expression)', 10
     first_message_len equ $ - first_message
 
+section .bss
+    input resb 100              ;stores user input
+
 section .text
     global _start                                          
 _start:
@@ -15,6 +18,7 @@ _start:
     mov [msglen], rax
     call _print_string
 
+    call _get_user_input        ;get first user input
 
 exit:
     mov ebx, 0
@@ -23,7 +27,7 @@ exit:
 
 ;-------------------------------
 
-_print_string:                  ;simply prints the string with length msglen that its address stored in msg
+_print_string:                  ;simply print the string with length msglen that its address stored in msg
     
     mov eax, 4                  ;'write' system call
     mov ebx, 1                  ;file descriptor 1 = screen
@@ -34,4 +38,30 @@ _print_string:                  ;simply prints the string with length msglen tha
     
     ret
 
+;-------------------------------
+
+_get_user_input:                ;simply get user input and store it in the input array(max length = 100 character)
+
+    mov rax, 0                  ;'read' system call
+    mov rdi, 0                  ;standard input
+    mov rsi, input              ;input will be in input array 
+    mov rdx, 100                ;max length of input
+    
+    syscall                     ;call the kernel
+
+    ret
+
+;-------------------------------
+
+_clear_array:
+
+    mov rcx, 100
+    mov esi, input
+    clearloop:
+        xor r8, r8
+        mov [esi], r8b
+        inc esi
+    loop clearloop
+
+    ret
 ;-------------------------------

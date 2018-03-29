e4612746980cc8611d668d84008a6430aa41dbd1
Alireza <daban.am56@gmail.com>
2018-03-29 10:14:23 +0430

    Fix  some bugs in BubbleSort

--- Exercises/BubbleSort/bubble-sort.asm
+++ Exercises/BubbleSort/bubble-sort.asm
@@ -1,5 +1,5 @@
 section .data
-    source dw 10, 100, 8, 7, 103, 5, 300, 3, 2, 1     ;input numbers
+    source dw 10, 100, 8, 7, 103, 5, -20000, -3, 2, 1   ;input numbers
     len equ ($ - source)/2 
 section .bss
     array resw len
@@ -12,45 +12,36 @@ _start:
     mov esi, array
 
 transfer:
-    mov al, [edi]                               ;insert numbers in array
-    mov [esi], al
+    mov ax, [edi]                                       ;insert numbers in array
+    mov [esi], ax
 
     add esi, 2
     add edi, 2
 
     loop transfer
 
-    ; call bubblesort
-
-    mov cx, len
-    mov esi, array
-
-check:
-    mov al, [esi]
-foo:
-    add esi, 2
-    loop check
+    call bubblesort
 
 exit:
     mov ebx, 0
     mov eax, 1
     int 80h
 
-;-----------------------------------------------
+;-------------------------------------------------------
 
-bubblesort:                                     ;sort Array with start pointer _array_ and length _len_
+bubblesort:                                             ;sort Array with start pointer _array_ and length _len_
 
-    xor rcx, rcx                                ;initialize registers
+    xor rcx, rcx                                        ;initialize registers
     mov cx, len
     dec cx
 
-    mov r8, 1                                   ;return if len == 1
+    mov r8, 1                                           ;return if len == 1
     xor r9, r9
     mov r9w, len
     cmp r9w, r8w
     je return 
 
-mainloop:                                       ;main loop of bubble sort
+mainloop:                                               ;main loop of bubble sort
 
     mov esi, array
     
@@ -64,10 +55,10 @@ mainloop:                                       ;main loop of bubble sort
         xor r11, r11 
         mov r10w, [esi]
         mov r11w, [esi + 2]
-        cmp r10w, r11w
+        cmp r10w, r11w                                  ;check if [esi] > [esi + 2] (two consecutive numbers in array)
         jng endinnerloop
 
-        mov [esi], r11w
+        mov [esi], r11w                                 ;if [esi] > [esi + 2] then swap [esi], [esi + 2] (two consecutive numbers in array)
         mov [esi + 2], r10w
 
     endinnerloop:

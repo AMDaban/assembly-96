551816514508c24ac5c9ed0c1d3326799ba6ebeb
Alireza <daban.am56@gmail.com>
2018-03-30 19:08:32 +0430

    just for saving

--- Projects/Calculator/calculator.asm
+++ Projects/Calculator/calculator.asm
@@ -1,19 +1,23 @@
 section .data
-    msg dq 0                    ;string address which will be written
-    msglen dq 0                 ;string length which will be written
+    msg dq 0                                ;string address which will be written
+    msglen dq 0                             ;string length which will be written
 
     first_message db 'Simple Calculator: (write a simple expression)', 10
     first_message_len equ $ - first_message
 
+    exp_max_size equ 50                     ;expression max size
+    integer_max_size equ 19                 ;integer max size
+
 section .bss
-    input resb 40               ;user input
-    number_chars resb 19        ;characters that specify a number
+    input resb exp_max_size                 ;user input
+    number_chars resb integer_max_size      ;characters that specify a number
+    expression resb exp_max_size
 
 section .text
     global _start                                          
 _start:
 
-    mov rax, first_message      ;print first_message
+    mov rax, first_message                  ;print first_message
     mov [msg], rax
     mov rax, first_message_len
     mov [msglen], rax
@@ -22,54 +26,48 @@ _start:
     call _clear_input_array
     call _clear_number_chars_array
 
-    call _get_user_input        ;get first user input
+    call _get_user_input                    ;get first user input
 
-    ; mov rcx, 19
-    ; mov esi, number_chars
-    ; clearnumbercharsloop1:
-    ;     mov r8b, 9
-    ;     mov [esi], r8b
-    ;     add [esi], byte 48
-    ;     inc esi
-    ; loop clearnumbercharsloop1
+    call _copy_input_array_to_expression_array
 
-    call _string_to_integer
+    call _evaluate_string
+
 exit:
     mov ebx, 0
     mov eax, 1
     int 80h
 
-;-------------------------------
+;-------------------------------------------
 
-_print_string:                  ;simply print the string with length msglen that its address stored in msg
+_print_string:                              ;simply print the string with length msglen that its address stored in msg
     
-    mov eax, 4                  ;'write' system call
-    mov ebx, 1                  ;file descriptor 1 = screen
-    mov ecx, [msg]              ;string to write       
-    mov edx, [msglen]           ;length of string to write
+    mov eax, 4                              ;'write' system call
+    mov ebx, 1                              ;file descriptor 1 = screen
+    mov ecx, [msg]                          ;string to write       
+    mov edx, [msglen]                       ;length of string to write
  
-    int 80h                     ;call the kernel
+    int 80h                                 ;call the kernel
     
     ret
 
-;-------------------------------
+;-------------------------------------------
 
-_get_user_input:                ;simply get user input and store it in the input array(max length = 100 character)
+_get_user_input:                            ;simply get user input and store it in the input array(max length = 100 character)
 
-    mov rax, 0                  ;'read' system call
-    mov rdi, 0                  ;standard input
-    mov rsi, input              ;input will be in input array 
-    mov rdx, 40                 ;max length of input
+    mov rax, 0                              ;'read' system call
+    mov rdi, 0                              ;standard input
+    mov rsi, input                          ;input will be in input array 
+    mov rdx, exp_max_size                   ;max length of input
     
-    syscall                     ;call the kernel
+    syscall                                 ;call the kernel
 
     ret
 
-;-------------------------------
+;-------------------------------------------
 
-_clear_input_array:             ;clear input array
+_clear_input_array:                         ;clear input array
 
-    mov rcx, 40
+    mov rcx, exp_max_size
     mov esi, input
     clearinputloop:
         xor r8, r8
@@ -78,11 +76,11 @@ _clear_input_array:             ;clear input array
     loop clearinputloop
 
     ret
-;-------------------------------
+;-------------------------------------------
 
-_clear_number_chars_array:      ;clear number_chars array
+_clear_number_chars_array:                  ;clear number_chars array
 
-    mov rcx, 19
+    mov rcx, integer_max_size
     mov esi, number_chars
     clearnumbercharsloop:
         xor r8, r8
@@ -91,18 +89,18 @@ _clear_number_chars_array:      ;clear number_chars array
     loop clearnumbercharsloop
 
     ret
-;-------------------------------
+;-------------------------------------------
 
-_string_to_integer:             ;convert string which is in number_chars to (unsigned)integer(stored in rax)
+_string_to_integer:                         ;convert string which is in number_chars to (unsigned)integer(stored in rax)
 
     mov esi, number_chars       
-    mov rcx, 19
-    xor r8, r8                  ;store number of digits
+    mov rcx, integer_max_size
+    xor r8, r8                              ;store number of digits
 
     stringtointegerloop_countdigits:        
-                                ;count number of digits (will be stored in r8d)
+                                            ;count number of digits (will be stored in r8d)
         
-        cmp [esi], byte 0       ;check if [esi] is a number or not(if [esi] is 0 then it is not a number)
+        cmp [esi], byte 0                   ;check if [esi] is a number or not(if [esi] is 0 then it is not a number)
         
         je digitnotexists
         
@@ -120,8 +118,7 @@ _string_to_integer:             ;convert string which is in number_chars to (uns
     mov r9, 1
     xor r10, r10 
 
-    stringtointegerloop_mainloop:
-                                ;convert string to integer (overflow : rax <- 0 )
+    stringtointegerloop_mainloop:           ;convert string to integer (overflow : rax <- 0 )
 
         xor r11, r11 
         mov r11b, [esi]
@@ -150,4 +147,61 @@ _string_to_integer:             ;convert string which is in number_chars to (uns
 
     ret
 
-;-------------------------------
+;-------------------------------------------
+
+_evaluate_string:                           ;evaluate string stored in expression
+
+    mov rcx, exp_max_size
+    mov esi, expression
+
+    xor r8, r8
+    mov r8b, 0                              ;specify operation(1 -> +, 2 -> -, 3 -> *, 4 -> /)
+
+    xor r10, r10
+
+    evaluatestring_mainloop:
+        mov al, [esi]
+        cmp [esi], byte 0
+        je end_evaluatestring_mainloop
+
+        cmp [esi], byte 43                  ;+ read
+        mov r10, 1
+        cmove r8, r10
+
+        cmp [esi], byte 45                  ;- read
+        mov r10, 2
+        cmove r8, r10
+
+        cmp [esi], byte 42                  ;* read
+        mov r10, 3
+        cmove r8, r10
+
+        cmp [esi], byte 47                  ;/ read
+        mov r10, 4
+        cmove r8, r10
+
+    end_evaluatestring_mainloop:
+        inc esi
+    loop evaluatestring_mainloop
+
+    ret
+
+;-------------------------------------------
+_copy_input_array_to_expression_array:      ;copy input into expression
+
+    mov rcx, exp_max_size
+    mov esi, input
+    mov edi, expression
+    
+    copyinputarraytoexpressionarray_mainloop:
+
+    mov al, [esi]
+    mov [edi], al
+
+    inc esi
+    inc edi 
+
+    loop copyinputarraytoexpressionarray_mainloop
+
+    ret
+;-------------------------------------------

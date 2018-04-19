58cac8b964dcfd6d944e64cdd27db7148c8ff26e
Alireza <daban.am56@gmail.com>
2018-04-19 18:59:58 +0430

    QuickSort completed

--- Exercises/QuickSort/README.md
+++ Exercises/QuickSort/README.md
@@ -1,16 +1,16 @@
 # BCD TO Binary
-convert BCD to binary<br />
-_bcd_to_binary converts a 16 digit (or less) unsigned number stored in rax to binary and store it in rbx
+sorts an array<br />
+;_quick_sort sorts parameter 1(target array) from parameter 2(start index) to parameter 3(end index)
 
 ## Scripts
  1. run.sh: <br />
     compile, link and run the program <br />
     ```bash
-    cd <REPOSITORY_ROOT>/Exercises/BCDtoBinary/ && source run.sh
+    cd <REPOSITORY_ROOT>/Exercises/QuickSort/ && source run.sh
     ```
  
  2. run.sh: <br />
     compile, link the program and run it with gdb <br />
     ```bash
-    cd <REPOSITORY_ROOT>/Exercises/BCDtoBinary/ && source run-with-gdb.sh
+    cd <REPOSITORY_ROOT>/Exercises/QuickSort/ && source run-with-gdb.sh
     ```
--- Exercises/QuickSort/quick-sort.asm
+++ Exercises/QuickSort/quick-sort.asm
@@ -1,6 +1,8 @@
+;_quick_sort sorts parameter 1(target array) from parameter 2(start index) to parameter 3(end index)
+
 section .data
     ;array to sort
-    source dw 990, 100, 8, 7, 103, 5, -20000, -3, 2, 876
+    source dw 10, -1, 9, -2, 8, -3, 7, -4, 6, -5, 5, -6, 4, -7, 3, -8, 2, -9, 1, -10, 1, 1, -1, -1
 
     ;array length 
     len equ ($ - source)/2
@@ -19,14 +21,24 @@ _start:
     mov rax, source                     
     push rax                            ;set first parameter(array pointer)
 
-    call _quick_sort
+    call _quick_sort                    
+
+    mov rcx, len
+    mov r8, source
+    check_loop:
+    mov bx, [r8]
+check:
+    inc r8
+    inc r8
+    loop check_loop
+    
 exit:
     mov ebx, 0
     mov eax, 1
     int 80h
 ;---------------------------------------
 
-_quick_sort:                            ;_quick_sort sorts an array witch rax points to.(with length bx) 
+_quick_sort:                            ;_quick_sort sorts parameter 1(target array) from parameter 2(start index) to parameter 3(end index) 
 
     enter 12, 0                         ;reserve space for local variables
 
@@ -38,14 +50,13 @@ _quick_sort:                            ;_quick_sort sorts an array witch rax po
 
     mov r8, [rbp + 16]
     mov [rbp - 12], r8                  ;[rbp - 12] -> array (64 bit)
-
     
-    mov r8w, [rbp - 4]                  ;base case(if end >=)
+    mov r8w, [rbp - 4]                  ;base case
     mov r9w, [rbp - 2]
     cmp r8w, r9w
     jg size_greater_than_zero
         leave
-        ret
+        ret 12
     size_greater_than_zero:
 
     mov ax, [rbp - 4]
@@ -58,6 +69,34 @@ _quick_sort:                            ;_quick_sort sorts an array witch rax po
     push rax                            ;set first parameter(array pointer)
  
     call _partition
+
+    dec ax
+
+    push ax
+
+    push ax                             ;set third parameter(end)
+
+    mov bx, [rbp - 2]
+    push bx                             ;set second parameter(start)
+
+    mov rbx, [rbp - 12]                     
+    push rbx                            ;set first parameter(array pointer)
+
+    call _quick_sort
+
+    pop ax
+
+    add ax, 2
+
+    mov bx, [rbp - 4]
+    push bx                             ;set third parameter(end)
+
+    push ax                             ;set second parameter(start)
+
+    mov rbx, [rbp - 12]                     
+    push rbx                            ;set first parameter(array pointer)
+
+    call _quick_sort
 
     leave
 
@@ -82,9 +121,67 @@ _partition:
     add r8w, [rbp - 4]
     add r8w, [rbp - 4]
     add r8, [rbp - 12]
-    mov ax, [r8]
+    mov r8w, [r8]                       ;pivot 
 
+    xor r9, r9
+    mov r9w, [rbp - 2]
+    dec r9w                             ;index of smaller element
+
+    xor r10, r10
+    mov r10w, [rbp - 2]                 ;start
     
+    xor r11, r11
+    mov r11w, [rbp - 4]                 ;end - 1
+    dec r11w
+
+    partition_main_loop:
+
+        cmp r10w, r11w
+        jg partition_out_of_loop
+
+        mov r12, [rbp - 12]
+        add r12, r10
+        add r12, r10
+        mov ax, [r12] 
+
+        cmp ax, r8w
+        jg not_greater
+            inc r9w                     ;increment index of smaller element
+            
+            mov r13, [rbp - 12]
+            add r13, r9
+            add r13, r9
+
+            mov bx, [r13]               ;swap [r13, r12]                       
+            mov cx, [r12]
+            mov [r13], cx
+            mov [r12], bx
+
+        not_greater:
+
+        inc r10w
+
+    jmp partition_main_loop
+
+    partition_out_of_loop:
+
+    inc r9w
+    
+    mov r14, [rbp - 12]
+    add r14, r9
+    add r14, r9
+
+    mov r15, [rbp - 12]
+    inc r11w
+    add r15, r11
+    add r15, r11
+    
+    mov ax, [r15]
+    mov bx, [r14]
+    mov [r15], bx
+    mov [r14], ax
+
+    mov ax, r9w
 
     leave
 
